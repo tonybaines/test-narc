a521d9efe716e9ce095f2b4eb6cc68043af71df2
Tony Baines <tony.baines.1@gmail.com>
2011-02-15 22:01:35 +0000

    Removed file-based logging

--- src/main/groovy/tonybaines/testnarc/Execute.groovy
+++ src/main/groovy/tonybaines/testnarc/Execute.groovy
@@ -9,34 +9,25 @@ public class Execute {
     static def LOG_FILE_NAME = "./test-narc.log"
   
     public static void main(String[] args) {
-        def cli = new CliBuilder(usage: ' -p "path/to/project/root" [-v] [-t]')
+        def cli = new CliBuilder(usage: ' -p "path/to/project/root" [-v]')
         def version = Version.VERSION
         cli.p(argName:'path', args:1, required:true, 'Path to project root')
         cli.v(argName:'verbose', longOpt: 'verbose', required:false, 'Verbose diagnostic messages')
-        cli.t(argName:'trace', longOpt: 'trace', required:false, 'Verbose logging messages')
     
         def startTime = Calendar.instance.timeInMillis
         def opt = cli.parse(args)
-        if (opt) {
-            RollingFileAppender logFileAppender = new RollingFileAppender(LogInit.DEFAULT_LAYOUT, LOG_FILE_NAME, true)
-            logFileAppender.setMaxFileSize("1MB")
-            logFileAppender.maxBackupIndex = 1
-      
+        if (opt) {      
             def consoleLogLevel = opt.v ? Level.DEBUG : Level.INFO
-            def fileLogLevel = opt.t ? Level.DEBUG : Level.INFO
       
-            LogInit.initialise(appenders: [[LogInit.DEFAULT_CONSOLE_APPENDER, consoleLogLevel],
-                    [logFileAppender, fileLogLevel]])
+            LogInit.initialise(appenders: [[LogInit.DEFAULT_CONSOLE_APPENDER, consoleLogLevel]])
                                      
             def log = Logger.getLogger("MAIN")
-            log.info "You are running application version ${Version.VERSION}"
+            log.info "Running application version ${Version.VERSION}"
       
             try {
                 def path = new File(opt.p)
         
                 if (path.exists()) {
-                    
-
                     TestNarc testNarc = new TestNarc()
 					testNarc.check(path.absolutePath).each {
 						log.info it
